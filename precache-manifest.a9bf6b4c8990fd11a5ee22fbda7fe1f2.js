3a4e5112007af6785da6ce2bf60eb0008528112e
Bernard Iorver <bernard.benion.iorver@hotmail.com>
2020-03-30 03:50:11 +0100

    Updates

---
+++ precache-manifest.a9bf6b4c8990fd11a5ee22fbda7fe1f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107248da7074181b775f633d4ce2fb25",
+    "url": "/benion-expense-tracker-react/index.html"
+  },
+  {
+    "revision": "3f9c4ef7830d8830447b",
+    "url": "/benion-expense-tracker-react/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "a23a6166b09832f35472",
+    "url": "/benion-expense-tracker-react/static/js/2.12a24628.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/benion-expense-tracker-react/static/js/2.12a24628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9c4ef7830d8830447b",
+    "url": "/benion-expense-tracker-react/static/js/main.44eb38b4.chunk.js"
+  },
+  {
+    "revision": "ff149227ec720a147e69",
+    "url": "/benion-expense-tracker-react/static/js/runtime-main.4a56a8d9.js"
+  }
+]);
